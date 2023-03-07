cedbf3e3041081e30319eef14cb87bdd86a21651
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-03-07 14:49:22 -0700

    deleted unnecessary tabs

--- nested_dictionaries_and_lists.py
+++ nested_dictionaries_and_lists.py
@@ -23,10 +23,10 @@ z[0]['y'] = 30
 print(z)
 
 students = [
-         {'first_name':  'Michael', 'last_name' : 'Jordan'},
-         {'first_name' : 'John', 'last_name' : 'Rosales'},
-         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
 
 def iterateDictionary(sourceList):
